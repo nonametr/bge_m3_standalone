e33bed1dfb642c8744fac705ff936d3bba9c86c2
Igor K. <igor.kuruch@shopit.se>
2024-09-13 14:50:22 +0200

    run in screen

--- bge_m3/run.sh
+++ bge_m3/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#python3 bge_m3_standalone.py
-gunicorn -b 0.0.0.0:8093 -w 1 --threads 2 --log-level "debug" bge_m3_standalone:app
+screen -dmS www gunicorn -b 0.0.0.0:8093 -w 1 --threads 2 --log-level "debug" bge_m3_standalone:app
